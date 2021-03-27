bd4d3a9762f561384a7a4bf0d713ca9cbd7e6fc4
Ibnu Halim Mustofa <ibnuhalimm@gmail.com>
2021-03-27 08:54:33 +0700

    change bg color of home button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,7 +18,7 @@ function Home() {
             <div className="mt-10 flex flex-row items-center justify-center">
                 <Link
                     to="/"
-                    className="mx-2 p-5 rounded-lg border border-solid border-indigo-800 text-indigo-800 hover:bg-indigo-800 hover:text-white transition duration-300">
+                    className="mx-2 p-5 rounded-lg border border-solid border-indigo-800 bg-indigo-800 text-white transition duration-300">
                         <svg className="w-10 h-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
                         </svg>
