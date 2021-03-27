3597aa92bb5386c7f0335bab1126e1c740568bc6
Ibnu Halim Mustofa <ibnuhalimm@gmail.com>
2021-03-27 13:02:37 +0700

    fix base_url api

--- src/constant/API.js
+++ src/constant/API.js
@@ -1,4 +1,4 @@
-const BASE_URL_API = process.env.REACT_APP_BASE_URL_API + '/api.php';
+const BASE_URL_API = process.env.REACT_APP_BASE_URL_API;
 
 const API = {
     GET_CUSTOMERS: BASE_URL_API + '/records/customers'
